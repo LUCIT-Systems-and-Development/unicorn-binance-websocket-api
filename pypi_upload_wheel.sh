ca58e1df8999b0be716d4d18f41654484643b762
Oliver Zehentleitner <47597331+bithon@users.noreply.github.com>
2019-04-09 16:10:39 +0200

    Update pypi_upload_wheel.sh

--- pypi_upload_wheel.sh
+++ pypi_upload_wheel.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
+# ~/.pypirc
+#[distutils]
+#index-servers=pypi
+#[pypi]
+#repository = https://upload.pypi.org/legacy/
+#username =unicorn_data_analysis
+
 python -m twine upload dist/*
