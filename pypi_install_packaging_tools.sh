84835327a972e66a27ed5fb265be2f4683152639
Oliver Zehentleitner <oliver.zehentleitner@unicorn-data.com>
2019-04-12 23:14:43 +0200

    adding pypi_install_packaging_tools.sh

---
+++ pypi_install_packaging_tools.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+#
+# File: pypi_install_packaging_tools.sh
+#
+# Part of ‘UNICORN Binance WebSocket API’
+# Project website: https://github.com/unicorn-data-analysis/unicorn-binance-websocket-api
+# Documentation: https://www.unicorn-data.com/unicorn-binance-websocket-api.html
+# PyPI: https://pypi.org/project/unicorn-binance-websocket-api/
+#
+# Author: UNICORN Data Analysis
+#         https://www.unicorn-data.com/
+#
+# Copyright (c) 2019, UNICORN Data Analysis
+# All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+python -m pip install --user --upgrade pip setuptools wheel twine tqdm
