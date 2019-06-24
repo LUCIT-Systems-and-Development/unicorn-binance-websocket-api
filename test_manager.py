cefdbc7307e68cc43bdc1c545a4fed0038127394
Oliver Zehentleitner <oliver.zehentleitner@unicorn-data.com>
2019-06-24 20:01:05 +0200

    added test

--- test_manager.py
+++ test_manager.py
@@ -4,15 +4,29 @@ from unicorn_binance_websocket_api.unicorn_binance_websocket_api_manager import 
 import unittest
 
 
-class TestBinanceManager(unittest.TestCase):
+class TestManagerBinanceCom(unittest.TestCase):
 
-    def test_create_uri(self):
-        binance_websocket_api_manager = BinanceWebSocketApiManager(exchange="binance.com")
-        self.assertEqual(binance_websocket_api_manager.create_websocket_uri(["arr"], ["!ticker"]),
+    def setUp(self):
+        self.binance_websocket_api_manager = BinanceWebSocketApiManager(exchange="binance.com")
+
+    def test_create_uri_ticker_regular(self):
+        self.assertEqual(self.binance_websocket_api_manager.create_websocket_uri(["!ticker"], ["arr"]),
                          'wss://stream.binance.com:9443/ws/!ticker@arr/')
-        self.assertEqual(binance_websocket_api_manager.create_websocket_uri(["!ticker"], ["arr"]),
+
+    def test_create_uri_ticker_reverse(self):
+        self.assertEqual(self.binance_websocket_api_manager.create_websocket_uri(["arr"], ["!ticker"]),
                          'wss://stream.binance.com:9443/ws/!ticker@arr/')
-        binance_websocket_api_manager.stop_manager_with_all_streams()
+
+    def test_create_uri_miniticker_regular(self):
+        self.assertEqual(self.binance_websocket_api_manager.create_websocket_uri(["!miniTicker"], ["arr"]),
+                         'wss://stream.binance.com:9443/ws/!miniTicker@arr')
+
+    def test_create_uri_miniticker_reverse(self):
+        self.assertEqual(self.binance_websocket_api_manager.create_websocket_uri(["arr"], ["!miniTicker"]),
+                         'wss://stream.binance.com:9443/ws/!miniTicker@arr')
+
+    def tearDown(self):
+        self.binance_websocket_api_manager.stop_manager_with_all_streams()
 
 
 if __name__ == '__main__':
