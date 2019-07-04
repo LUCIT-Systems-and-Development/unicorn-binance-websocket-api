229f68f2ca1969765b8d93733da743a385431076
Oliver Zehentleitner <oliver.zehentleitner@unicorn-data.com>
2019-07-04 18:40:41 +0200

    add dev_test_dex_print.py

---
+++ dev_test_dex_print.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# File: example_binance_dex.py
+#
+# Part of ‘UNICORN Binance WebSocket API’
+# Project website: https://github.com/unicorn-data-analysis/unicorn-binance-websocket-api
+# Documentation: https://www.unicorn-data.com/unicorn-binance-websocket-api.html
+# PyPI: https://pypi.org/project/unicorn-binance-websocket-api/
+#
+# Author: UNICORN Data Analysis
+#         https://www.unicorn-data.com/
+#
+# Copyright (c) 2019, UNICORN Data Analysis
+# All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+from unicorn_binance_websocket_api.unicorn_binance_websocket_api_manager import BinanceWebSocketApiManager
+from unicorn_fy import UnicornFy
+import logging
+import time
+import threading
+import os
+
+
+# https://docs.python.org/3/library/logging.html#logging-levels
+logging.basicConfig(filename=os.path.basename(__file__) + '.log',
+                    format="{asctime} [{levelname:8}] {process} {thread} {module}: {message}",
+                    style="{")
+logging.getLogger('unicorn-log').addHandler(logging.StreamHandler())
+logging.getLogger('unicorn-log').setLevel(logging.INFO)
+
+
+def print_stream_data_from_stream_buffer(binance_websocket_api_manager):
+    while True:
+        if binance_websocket_api_manager.is_manager_stopping():
+            exit(0)
+        oldest_stream_data_from_stream_buffer = binance_websocket_api_manager.pop_stream_data_from_stream_buffer()
+        if oldest_stream_data_from_stream_buffer is False:
+            time.sleep(0.01)
+        else:
+            unicorn_fied_stream_data = UnicornFy.binance_org_websocket(oldest_stream_data_from_stream_buffer)
+            print(unicorn_fied_stream_data)
+
+
+# create instance of BinanceWebSocketApiManager for Binance Chain DEX
+binance_websocket_api_manager = BinanceWebSocketApiManager(exchange="binance.org")
+
+# start a worker process to move the received stream_data from the stream_buffer to a print function
+worker_thread = threading.Thread(target=print_stream_data_from_stream_buffer, args=(binance_websocket_api_manager,))
+worker_thread.start()
+
+# userAddress streams
+binance_dex_user_address = ""
+id = binance_websocket_api_manager.create_stream('orders', binance_dex_user_address)
+binance_websocket_api_manager.create_stream('accounts', binance_dex_user_address)
+binance_websocket_api_manager.create_stream('transfers', binance_dex_user_address)
+user_address_multi_stream_id = binance_websocket_api_manager.create_stream(['orders', 'transfers', 'accounts'],
+                                                                           binance_dex_user_address)
+
+binance_websocket_api_manager.create_stream(["allTickers"], ["$all"])
+binance_websocket_api_manager.create_stream(["allMiniTickers"], ["$all"])
+binance_websocket_api_manager.create_stream(["blockheight"], ["$all"])
+
+markets = 'RAVEN-F66_BNB'
+binance_websocket_api_manager.create_stream(["trades"], markets)
+binance_websocket_api_manager.create_stream(["marketDepth"], markets)
+binance_websocket_api_manager.create_stream(["kline_1h"], markets)
+binance_websocket_api_manager.create_stream(["kline_1h"], markets)
+binance_websocket_api_manager.create_stream(["ticker"], markets)
+binance_websocket_api_manager.create_stream(["miniTicker"], markets)
+
+markets = ['RAVEN-F66_BNB', 'ANKR-E97_BNB', 'AWC-986_BNB', 'COVA-218_BNB', 'BCPT-95A_BNB', 'WISH-2D5_BNB',
+           'MITH-C76_BNB', 'BNB_BTCB-1DE', 'BNB_USDSB-1AC', 'BTCB-1DE_USDSB-1AC']
+channels = ['trades', 'kline_1m', 'kline_5m', 'kline_15m', 'marketDepth', 'ticker', 'miniTicker', 'marketDiff']
+multiplex_stream_id = binance_websocket_api_manager.create_stream(channels, markets)
