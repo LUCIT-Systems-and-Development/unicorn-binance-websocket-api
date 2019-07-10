d0fbcac057ffbdb12878df2f519a650e78b16b78
Oliver Zehentleitner <oliver.zehentleitner@unicorn-data.com>
2019-07-10 22:00:31 +0200

    dev stage

--- unicorn_binance_websocket_api/unicorn_binance_websocket_api_manager.py
+++ unicorn_binance_websocket_api/unicorn_binance_websocket_api_manager.py
@@ -78,7 +78,7 @@ class BinanceWebSocketApiManager(threading.Thread):
 
     def __init__(self, process_stream_data=False, exchange="binance.com"):
         threading.Thread.__init__(self)
-        self.version = "1.6.3"
+        self.version = "1.6.3.dev"
         if process_stream_data is False:
             # no special method to process stream data provided, so we use write_to_stream_buffer:
             self.process_stream_data = self.add_to_stream_buffer
