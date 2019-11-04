c572e890c351fd208a9ae322f741deb8144ca76b
Oliver Zehentleitner <oliver.zehentleitner@unicorn-data.com>
2019-11-04 14:09:42 +0100

    moved docs to github pages

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='unicorn-binance-websocket-api',
-     version='1.8.0-dev',
+     version='1.8.1.dev',
      author="Oliver Zehentleitner",
      url="https://about.me/oliver-zehentleitner/",
      scripts=['unicorn_binance_websocket_api.py'],
