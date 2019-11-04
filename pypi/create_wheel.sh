f41f1a6f189c91859253de7558a2601f0e63e16f
Oliver Zehentleitner <oliver.zehentleitner@unicorn-data.com>
2019-11-04 14:15:30 +0100

    update pypi

--- pypi/create_wheel.sh
+++ pypi/create_wheel.sh
@@ -34,7 +34,7 @@
 # IN THE SOFTWARE.
 
 security-check() {
-    echo -n "Did you change the version in \`setup.py\` and \`unicorn_binance_websocket_api_manager.py\`? [yes|NO] "
+    echo -n "Did you change the version in \`setup.py\`, \`sphinx conf.py\` and \`unicorn_binance_websocket_api_manager.py\`? [yes|NO] "
     local SURE
     read SURE
     if [ "$SURE" != "yes" ]; then
