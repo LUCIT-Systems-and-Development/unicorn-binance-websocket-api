925d03a51ccef52737df8d0932faf7067901abf8
Oliver Zehentleitner <oliver.zehentleitner@unicorn-data.com>
2020-02-13 16:32:13 +0100

    update docs

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 # UNICORN Binance WebSocket API
 A python API to use the Binance Websocket API's (com, com-margin, com-futures, jersey, us, dex/chain+testnet) in a easy, fast, flexible, robust and fully-featured way.
 
-### Create a multiplex websocket connection to Binance with just 3 lines of code:
+### [Create a multiplex websocket connection](https://oliver-zehentleitner.github.io/unicorn-binance-websocket-api/unicorn_binance_websocket_api.html#unicorn_binance_websocket_api.unicorn_binance_websocket_api_manager.BinanceWebSocketApiManager.create_stream) to Binance with just 3 lines of code:
 ```
 from unicorn_binance_websocket_api.unicorn_binance_websocket_api_manager import BinanceWebSocketApiManager
 
--- unicorn_binance_websocket_api/unicorn_binance_websocket_api_manager.py
+++ unicorn_binance_websocket_api/unicorn_binance_websocket_api_manager.py
@@ -644,11 +644,14 @@ class BinanceWebSocketApiManager(threading.Thread):
         other streams in a multiplexed stream!
 
         Example:
+
         Set api_key and api_secret:
-        `binance_websocket_api_manager.set_private_api_config(binance_api_key, binance_api_secret)`
+
+        ``binance_websocket_api_manager.set_private_api_config(binance_api_key, binance_api_secret)``
 
         Then start the stream:
-        `userdata_stream_id = binance_websocket_api_manager.create_stream(["arr"], ["!userData"])`
+
+        ``userdata_stream_id = binance_websocket_api_manager.create_stream(["arr"], ["!userData"])``
 
         To create a multiplexed stream which includes also `!miniTicker@arr`, `!ticker@arr` or `!bookTicker@arr` you
         just need to add `!miniTicker` to the cannels list - dont add `arr` to the markets list.
@@ -657,7 +660,7 @@ class BinanceWebSocketApiManager(threading.Thread):
 
         Example:
 
-        `binance_websocket_api_manager.subscribe_to_stream(stream_id, channels=['kline_5m', 'marketDepth', '!miniTicker'])`
+        ``binance_websocket_api_manager.subscribe_to_stream(stream_id, channels=['kline_5m', 'marketDepth', '!miniTicker'])``
 
         :param channels: provide the channels you wish to stream
         :type channels: str, tuple, list, set
