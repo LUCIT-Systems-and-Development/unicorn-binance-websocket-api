bd8fe8d5ab9a28836a4dcf4cf611bb371133aace
Oliver Zehentleitner <oliver.zehentleitner@unicorn-data.com>
2020-06-19 18:12:12 +0200

    dev version & docs

--- unicorn_binance_websocket_api/unicorn_binance_websocket_api_manager.py
+++ unicorn_binance_websocket_api/unicorn_binance_websocket_api_manager.py
@@ -124,7 +124,7 @@ class BinanceWebSocketApiManager(threading.Thread):
                  throw_exception_if_unrepairable=False,
                  restart_timeout=6):
         threading.Thread.__init__(self)
-        self.version = "1.16.3"
+        self.version = "1.16.3.dev"
         logging.info("New instance of unicorn_binance_websocket_api_manager " + self.version + " started ...")
         colorama.init()
         if process_stream_data is False:
