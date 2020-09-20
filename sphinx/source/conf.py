2b68edd02a7852f4f28155c67eaf2c2db08c37c8
oliver <oliver@zehentleitner.co>
2020-09-20 19:27:05 +0200

    1.17.2

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -27,9 +27,9 @@ author = 'Oliver Zehentleitner'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.17.1'
+release = '1.17.2'
 
-html_last_updated_fmt = "Fr 18. Sep 21:07:02 UTC 2020"
+html_last_updated_fmt = "So 20. Sep 17:24:35 UTC 2020"
 
 # -- General configuration ---------------------------------------------------
 
