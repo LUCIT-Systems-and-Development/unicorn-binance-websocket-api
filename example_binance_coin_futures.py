8911e61fcd39a7dd4e4ebf7a86c2310766f4e9fa
M3tz3l <f.metzler@posteo.de>
2021-04-14 00:16:07 +0200

    Add coin-m futures usage example

---
+++ example_binance_coin_futures.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# File: example_binance_futures.py
+#
+# Part of ‘UNICORN Binance WebSocket API’
+# Project website: https://github.com/oliver-zehentleitner/unicorn-binance-websocket-api
+# Documentation: https://oliver-zehentleitner.github.io/unicorn-binance-websocket-api
+# PyPI: https://pypi.org/project/unicorn-binance-websocket-api/
+#
+# Author: Oliver Zehentleitner
+#         https://about.me/oliver-zehentleitner
+#
+# Copyright (c) 2019-2021, Oliver Zehentleitner
+# All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+from unicorn_binance_websocket_api.unicorn_binance_websocket_api_manager import BinanceWebSocketApiManager
+import logging
+import time
+import threading
+import os
+
+# https://docs.python.org/3/library/logging.html#logging-levels
+logging.basicConfig(level=logging.DEBUG,
+                    filename=os.path.basename(__file__) + '.log',
+                    format="{asctime} [{levelname:8}] {process} {thread} {module}: {message}",
+                    style="{")
+
+
+def print_stream_data_from_stream_buffer(binance_websocket_api_manager):
+    while True:
+        if binance_websocket_api_manager.is_manager_stopping():
+            exit(0)
+        oldest_stream_data_from_stream_buffer = binance_websocket_api_manager.pop_stream_data_from_stream_buffer()
+        if oldest_stream_data_from_stream_buffer is False:
+            time.sleep(0.01)
+
+# create instance of BinanceWebSocketApiManager for Binance.com Futures
+binance_websocket_api_manager = BinanceWebSocketApiManager(exchange="binance.com-coin-futures")
+
+# set api key and secret for userData stream
+binance_je_api_key = ""
+binance_je_api_secret = ""
+userdata_stream_id = binance_websocket_api_manager.create_stream(["arr"],
+                                                                 ["!userData"],
+                                                                 api_key=binance_je_api_key,
+                                                                 api_secret=binance_je_api_secret)
+
+bookticker_all_stream_id = binance_websocket_api_manager.create_stream(["arr"], ["!bookTicker"])
+
+# https://binance-docs.github.io/apidocs/delivery/en/#mark-price-of-all-symbols-of-a-pair
+stream_id = binance_websocket_api_manager.create_stream(["markPrice@1s"], "btcusd", stream_label="BTCUSD@arr@1s")
+
+symbols = {'btcusd_perp', 'ethusd_perp', 'bnbusd_perp'}
+pairs = {'btcusd', 'ethusd', 'bnbusd'}
+binance_websocket_api_manager.create_stream(["aggTrade"], symbols)
+binance_websocket_api_manager.create_stream(["markPrice"], pairs)
+binance_websocket_api_manager.create_stream(["markPriceKline_1m"], symbols)
+binance_websocket_api_manager.create_stream(["indexPriceKline_5m"], symbols)
+binance_websocket_api_manager.create_stream(["depth5@100ms"], symbols)
+binance_websocket_api_manager.create_stream(["depth10"], symbols)
+
+# start a worker process to move the received stream_data from the stream_buffer to a print function
+worker_thread = threading.Thread(target=print_stream_data_from_stream_buffer, args=(binance_websocket_api_manager,))
+worker_thread.start()
+
+# show an overview
+while True:
+    binance_websocket_api_manager.print_summary()
+    time.sleep(1)
