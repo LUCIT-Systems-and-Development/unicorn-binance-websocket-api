52762b6b5ff362e9ccfecd5b08702633acc36613
oliver <oliver@zehentleitner.co>
2021-09-19 22:36:16 +0200

    ubra/ubwa

--- example_stream_everything.py
+++ example_stream_everything.py
@@ -73,43 +73,43 @@ def print_stream_data_from_stream_buffer(binance_websocket_api_manager):
             time.sleep(0.01)
 
 try:
-    binance_rest_client = unicorn_binance_rest_api.BinanceRestApiManager(binance_api_key, binance_api_secret)
+    ubra = unicorn_binance_rest_api.BinanceRestApiManager(binance_api_key, binance_api_secret)
 except requests.exceptions.ConnectionError:
     print("No internet connection?")
     sys.exit(1)
 
-binance_websocket_api_manager = unicorn_binance_websocket_api.BinanceWebSocketApiManager()
+ubwa = unicorn_binance_websocket_api.BinanceWebSocketApiManager()
 
 # start a worker process to move the received stream_data from the stream_buffer to a print function
-worker_thread = threading.Thread(target=print_stream_data_from_stream_buffer, args=(binance_websocket_api_manager,))
+worker_thread = threading.Thread(target=print_stream_data_from_stream_buffer, args=(ubwa,))
 worker_thread.start()
 
 markets = []
-data = binance_rest_client.get_all_tickers()
+data = ubra.get_all_tickers()
 for item in data:
     markets.append(item['symbol'])
 
-private_stream_id_alice = binance_websocket_api_manager.create_stream(["!userData"],
-                                                                      ["arr"],
-                                                                      api_key=binance_api_key,
-                                                                      api_secret=binance_api_secret,
-                                                                      stream_label="userData Alice")
+private_stream_id_alice = ubwa.create_stream(["!userData"],
+                                             ["arr"],
+                                             api_key=binance_api_key,
+                                             api_secret=binance_api_secret,
+                                             stream_label="userData Alice")
 
-private_stream_id_bob = binance_websocket_api_manager.create_stream(["!userData"],
-                                                                    ["arr"],
-                                                                    api_key="aaa",
-                                                                    api_secret="bbb",
-                                                                    stream_label="userData Bob")
+private_stream_id_bob = ubwa.create_stream(["!userData"],
+                                           ["arr"],
+                                           api_key="aaa",
+                                           api_secret="bbb",
+                                           stream_label="userData Bob")
 
-arr_stream_id = binance_websocket_api_manager.create_stream(arr_channels, "arr", stream_label="arr channels",
-                                                            ping_interval=10, ping_timeout=10, close_timeout=5)
+arr_stream_id = ubwa.create_stream(arr_channels, "arr", stream_label="arr channels",
+                                   ping_interval=10, ping_timeout=10, close_timeout=5)
 
-divisor = math.ceil(len(markets) / binance_websocket_api_manager.get_limit_of_subscriptions_per_stream())
+divisor = math.ceil(len(markets) / ubwa.get_limit_of_subscriptions_per_stream())
 max_subscriptions = math.ceil(len(markets) / divisor)
 
 for channel in channels:
     if len(markets) <= max_subscriptions:
-        binance_websocket_api_manager.create_stream(channel, markets, stream_label=channel)
+        ubwa.create_stream(channel, markets, stream_label=channel)
     else:
         loops = 1
         i = 1
@@ -117,14 +117,14 @@ for channel in channels:
         for market in markets:
             markets_sub.append(market)
             if i == max_subscriptions or loops*max_subscriptions + i == len(markets):
-                binance_websocket_api_manager.create_stream(channel, markets_sub, stream_label=str(channel+"_"+str(i)),
-                                                            ping_interval=10, ping_timeout=10, close_timeout=5)
+                ubwa.create_stream(channel, markets_sub, stream_label=str(channel+"_"+str(i)),
+                                   ping_interval=10, ping_timeout=10, close_timeout=5)
                 markets_sub = []
                 i = 1
                 loops += 1
             i += 1
 
 while True:
-    binance_websocket_api_manager.print_summary()
-    #binance_websocket_api_manager.print_stream_info(arr_stream_id)
+    ubwa.print_summary()
+    #ubwa.print_stream_info(arr_stream_id)
     time.sleep(1)
