256d23b8cc251a7c2f07efb50e6bf4f3d1e03776
oliver <oliver@zehentleitner.co>
2022-03-11 11:27:24 +0100

    file name

--- dev_test_callback.py
+++ dev_test_callback.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# File: dev_test_cex_full_non_stop.py
+# File: dev_test_callback.py
 #
 # Part of ‘UNICORN Binance WebSocket API’
 # Project website: https://www.lucit.tech/unicorn-binance-websocket-api.html
