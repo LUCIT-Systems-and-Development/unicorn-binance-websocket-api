171d68b046d55b716a499dc239552767eb8e3a81
oliver <oliver@zehentleitner.co>
2022-03-18 22:45:55 +0100

    conda

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -27,7 +27,7 @@ author = 'LUCIT Systems and Development'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.40.6'
+release = '1.40.7'
 
 html_last_updated_fmt = "%b %d %Y at %H:%M (CET)"
 
