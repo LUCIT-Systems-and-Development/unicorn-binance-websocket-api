a038f684b5d8476f8481b4ff4f8d054af767949a
Oliver <oliver@zehentleitner.co>
2023-04-10 04:05:12 +0200

    dev version

--- unicorn_binance_websocket_api/manager.py
+++ unicorn_binance_websocket_api/manager.py
@@ -236,7 +236,7 @@ class BinanceWebSocketApiManager(threading.Thread):
                  socks5_proxy_ssl_verification: Optional[bool] = True,):
         threading.Thread.__init__(self)
         self.name = "unicorn-binance-websocket-api"
-        self.version = "1.45.2"
+        self.version = "1.45.2.dev"
         logger.info(f"New instance of {self.get_user_agent()} on "
                     f"{str(platform.system())} {str(platform.release())} for exchange {exchange} started ...")
         self.debug = debug
