ddc1dd5f78b0611757bdc338e54ef752fae643af
Oliver <oliver@zehentleitner.co>
2023-11-23 13:35:29 +0100

    release 2.0.0

--- unittest_binance_websocket_api2.py
+++ unittest_binance_websocket_api2.py
@@ -10,32 +10,20 @@
 # PyPI: https://pypi.org/project/unicorn-binance-websocket-api
 # LUCIT Online Shop: https://shop.lucit.services/software
 #
+# License: LSOSL - LUCIT Synergetic Open Source License
+# https://github.com/LUCIT-Systems-and-Development/unicorn-binance-websocket-api/blob/master/LICENSE
+#
 # Author: LUCIT Systems and Development
 #
 # Copyright (c) 2019-2023, LUCIT Systems and Development (https://www.lucit.tech)
 # All rights reserved.
-#
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish, dis-
-# tribute, sublicense, and/or sell copies of the Software, and to permit
-# persons to whom the Software is furnished to do so, subject to the fol-
-# lowing conditions:
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
-# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
-# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-# IN THE SOFTWARE.
-import time
+
 
 from unicorn_binance_websocket_api.manager import BinanceWebSocketApiManager
 import logging
 import unittest
 import os
+import time
 import threading
 
 import tracemalloc
