28be07d429f0e6d6d63e2e34a495eb6377bf7128
Oliver <oliver@zehentleitner.co>
2024-05-11 17:00:19 +0200

    2.6.1 RC1

--- examples/ubwa-demo/ubwa-demo.py
+++ examples/ubwa-demo/ubwa-demo.py
@@ -38,7 +38,7 @@ class BinanceDataProcessor:
         channels: list = ['trade', 'kline_1m', 'depth20']
         for channel in channels:
             self.ubwa.create_stream(channels=channel,
-                                    markets=markets[:50],
+                                    markets=markets[:20],
                                     process_asyncio_queue=self.process_data,
                                     stream_label=channel)
 
